466833dda6f648a29af226b6c2abce7f220f56b2
Collin-St <collin.street.dr@gmail.com>
2019-02-01 15:43:56 -0800

    basic scaffolding - react app and server

---
+++ challenge_3/client/src/App.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <h5>hello, world</h5>
+    )
+  }
+}
+
+export default App;

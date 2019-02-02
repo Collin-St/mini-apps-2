77c6243da4e553fb0a905a1cc723fae9706833b9
Collin-St <collin.street.dr@gmail.com>
2019-02-01 19:15:56 -0800

    added keypad

--- challenge_3/client/src/App.jsx
+++ challenge_3/client/src/App.jsx
@@ -8,7 +8,10 @@ class App extends Component {
 
   render() {
     return(
-      <h5>hello, world</h5>
+      <div>
+        <h5>hello, world</h5>
+        <input type="number" placeholder="Enter number of pins hit" data-role="keypad" data-open="true" data-size-as-keys="false" data-keys="1,2,3,4,5,6,7,8,9,10" autoFocus/>
+      </div>
     )
   }
 }
